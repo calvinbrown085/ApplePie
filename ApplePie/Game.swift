d5700a38cd673b3f111e78f364e7b6205a1ae2c4
Calvin Brown <calvin.brown@banno.com>
2018-08-08 06:47:19 -0500

    First Commit

---
+++ ApplePie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Calvin Brown on 7/31/18.
+//  Copyright © 2018 Calvin Brown. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            }
+            else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        print(letter)
+        print(word)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
